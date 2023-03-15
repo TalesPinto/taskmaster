e5c08ef23ecc2759502be036985b01010a49f4ed
Tales Pinto <tales.pinto@hotmail.com>
2023-03-15 14:26:38 +1100

    trying to set delete feature up

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -12,36 +12,50 @@ router.get('/dashboard', ensureLoggedIn, (req, res) => {
     const sql = 'SELECT * FROM tasks WHERE (user_id_assigner = $1) OR (user_id_assignee = $1);'
     db.query(sql, [req.session.userId], (err, dbRes) => {
         let tasks = dbRes.rows // dbRes.rows give us an ARRAY
-        // let username = req.session.userId
         res.render('dashboard', { tasks })
     })
 })
+
+router.delete('/tasks', (req, res) => {
+    console.log('cp1')
+    console.log(req.body.id)
+    const sql = `DELETE FROM tasks WHERE id = $1;`
+    db.query(sql, [req.body.id], (err, dbRes) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect(`/dashboard`)
+        }
+    })
+})
+
 
 router.get('/tasks', ensureLoggedIn, (req, res) => {
     console.log(req.session.userId)
     res.render('new_task')
 })
-// router.get('/tasks/:id', ensureLoggedIn, (req, res) => {
-
-//     // $1, $2, etc...avoid sql injection attack at your db
-//     let sql = `SELECT * FROM tasks WHERE user_id_assigner = $1 OR user_id_assignee = $1;`
-//     // 
-//     db.query(sql, [req.session.userId], (err, dbRes) => {
-//         const dish = dbRes.rows[0] // [0] because .rows always returns an ARRAY
-//         res.render('dish_details', { dish })
-//     })
-// })
 
 router.post('/tasks/new', ensureLoggedIn, (req, res) => {
 
     const sql = `INSERT INTO tasks (title, description, status, comment, user_id_assigner, user_id_assignee) values ($1, $2, $3, $4, $5, $6);`
-    //req.body is the query to get info
 
     db.query(sql, [req.body.title, req.body.description, req.body.status, req.body.comment, req.session.userId, req.body.user_id_assignee], (err, dbRes) => {
         res.redirect('/dashboard')
     })
 })
 
-
+router.put('/tasks', (req, res) => {
+    const sql = `UPDATE tasks SET title = $1, description = $2, comment = $3, status = $4, user_id_assigner = $5 WHERE id = $6;`
+    // console.log(req.body.title)
+    // console.log(req.body.id)
+    db.query(sql, [req.body.title, req.body.description, req.body.comment, req.body.status, req.body.user_id_assigner, req.body.id], (err, dbRes) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(req.body.title)
+            res.redirect(`/dashboard`)
+        }
+    })
+})
 
 module.exports = router;
